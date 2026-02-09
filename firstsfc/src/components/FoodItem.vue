ab6505519983e0366c74ccec0c3670afbc6132a1
ApcACalucin <alcalucin@student.apc.edu.ph>
2026-02-09 06:29:18 +0000

    3rd update

--- firstsfc/src/components/FoodItem.vue
+++ firstsfc/src/components/FoodItem.vue
@@ -1,20 +1,9 @@
-<template>
-  <div>
-    <h2>{{ name }}</h2>
-    <p>{{ message }}</p>
-  </div>
-</template>
- 
-<script>
-export default {
-  data() {
-    return {
-      name: 'Apples',
-      message: 'I like apples'
-    }
+<style>
+  #app > div {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: lightgreen;
   }
-};
-</script>
- 
-<style></style>
- 
+</style>
