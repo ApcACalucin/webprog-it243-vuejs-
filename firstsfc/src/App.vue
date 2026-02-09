5431e7650bbf46cd46fd7885b6f769bad668c155
ApcACalucin <alcalucin@student.apc.edu.ph>
2026-02-09 06:22:50 +0000

    update

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,9 +1,10 @@
 <template>
-<h1> Food </h1>
-<Food-item/>
-<Food-item/>
-<Food-item/>
+  <h1>Food</h1>
+  <food-item/>
+  <food-item/>
+  <food-item/>
 </template>
-
-<script> </script>
-<style> </style>
+ 
+<script></script>
+ 
+<style></style>
