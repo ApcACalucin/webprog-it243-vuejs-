f741e6506857faedcc14cd255bfd403f8fdbbd15
ApcACalucin <alcalucin@student.apc.edu.ph>
2026-02-12 06:47:26 +0000

    web

--- firstsfc/src/components/PersonalProfile.vue
+++ firstsfc/src/components/PersonalProfile.vue
@@ -7,15 +7,15 @@
    <div> 2 Education / Achievements</div>
    <div> 3 Course</div>
    <div> 4 IT Experience</div>
-   <div> 5 Hbbies and Interests</div>
+   <div> 5 Hobbies and Interests</div>
    <div> 6 Goals in life / Dream</div>
    <div> 7  Picture Gallery</div>
 
  </section>
- <aside class="idebar">
+ <aside class="sidebar">
    </aside>
 </main>
-      <footer>Asia Pacifiv College</footer>
+      <footer>Asia Pacific College</footer>
     </div>
 </template>
 <script> </script>
