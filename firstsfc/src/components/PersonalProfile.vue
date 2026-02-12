88cf05208aa96f0bd1e8adf782e466dd2e4f4722
ApcACalucin <alcalucin@student.apc.edu.ph>
2026-02-12 06:35:02 +0000

    personal.vue

---
+++ firstsfc/src/components/PersonalProfile.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="container">
+ <header> <h1>Personal Profile Web page</h1> </header>
+ <main>
+   <section class="main-content">
+   <div> 1 About Me</div>
+   <div> 2 Education / Achievements</div>
+   <div> 3 Course</div>
+   <div> 4 IT Experience</div>
+   <div> 5 Hbbies and Interests</div>
+   <div> 6 Goals in life / Dream</div>
+   <div> 7  Picture Gallery</div>
+
+ </section>
+ <aside class="idebar">
+   </aside>
+</main>
+      <footer>Asia Pacifiv College</footer>
+    </div>
+</template>
+<script> </script>
+<style></style>
+
