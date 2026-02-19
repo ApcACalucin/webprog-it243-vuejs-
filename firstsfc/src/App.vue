6f5053805499fe83d4f7d3b9ed2c723e67cc6647
ApcACalucin <alcalucin@student.apc.edu.ph>
2026-02-19 06:41:11 +0000

    update2

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,13 +1,38 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { supabase } from './lib/supabaseClient' // Path to your connection file
+import PersonalProfile from './components/PersonalProfile.vue' // Path to the code you just sent
+
+const instruments = ref([])
+
+async function getInstruments() {
+  const { data } = await supabase.from('instruments').select()
+  instruments.value = data
+}
+
+onMounted(() => {
+  getInstruments()
+})
+</script>
+
 <template>
-  <PersonalProfile />
+  <div id="app-wrapper">
+    <PersonalProfile />
+
+    <hr class="divider" />
+
+    <section class="database-content">
+      <h2>Instruments List</h2>
+      <ul>
+        <li v-for="instrument in instruments" :key="instrument.id">
+          {{ instrument.name }}
+        </li>
+      </ul>
+    </section>
+  </div>
 </template>
 
-<script>
-import PersonalProfile from './components/PersonalProfile.vue'
-
-export default {
-  components: {
-    PersonalProfile
-  }
-}
-</script>
+<style>
+.divider { margin: 40px 0; border: 1px solid #eee; }
+.database-content { text-align: center; }
+</style>
